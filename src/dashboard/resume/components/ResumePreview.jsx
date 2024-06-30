f9f8709694333a18b65bbcd6733aee9990995fe7
Nayan Mahera <maheranayan513@gmail.com>
2024-06-30 14:18:37 +0530

    preview section done

--- src/dashboard/resume/components/ResumePreview.jsx
+++ src/dashboard/resume/components/ResumePreview.jsx
@@ -1,17 +1,31 @@
 import { ResumeInfoContext } from "@/context/ResumeInfoContext";
 import React, { useContext } from "react";
 import PersonalDetail from "./preview/PersonalDetail";
+import SummeryPreview from "./preview/SummeryPreview";
+import ProExperiencePreview from "./preview/ProExperiencePreview";
+import EducationalPreview from "./preview/EducationalPreview";
+import SkillPreview from "./preview/SkillPreview";
 
 const ResumePreview = () => {
   const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext);
   return (
-    <div className="shadow-lg">
+    <div
+      className="shadow-lg h-full p-14 border-t-[20px]"
+      style={{
+        borderColor: resumeInfo?.themeColor,
+      }}
+    >
       {/* Personal Detail */}
       <PersonalDetail resumeInfo={resumeInfo} />
       {/* Summery */}
+      <SummeryPreview resumeInfo={resumeInfo} />
       {/* Professional Experience */}
+      <ProExperiencePreview resumeInfo={resumeInfo} />
       {/* Educational */}
+      <EducationalPreview resumeInfo={resumeInfo} />
+
       {/* {Skills} */}
+      <SkillPreview resumeInfo={resumeInfo} />
     </div>
   );
 };
---
+++ src/dashboard/resume/components/preview/SummeryPreview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SummeryPreview = ({ resumeInfo }) => {
+  return (
+    <div>
+      <p>{resumeInfo?.summery}</p>
+    </div>
+  );
+};
+
+export default SummeryPreview;
