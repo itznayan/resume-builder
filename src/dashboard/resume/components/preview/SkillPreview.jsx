f9e02670245c6f48f0acc35a873f4e3bca214823
Nayan Mahera <maheranayan513@gmail.com>
2024-07-05 16:27:39 +0530

    skill rating fixed

--- src/dashboard/resume/components/preview/SkillPreview.jsx
+++ src/dashboard/resume/components/preview/SkillPreview.jsx
@@ -19,7 +19,7 @@ const SkillPreview = ({ resumeInfo }) => {
               <div
                 className="h-2 "
                 style={{
-                  backgroundColor: resumeInfo?.themeColor,
+                  backgroundColor: "black",
                   width: skill?.rating * 20 + "%",
                 }}
               ></div>
